cec3a516d4058d903e86085f8c3ea5222eaeb31f
s-yasuoka <migayasu@gmail.com>
2022-05-28 08:05:39 +0000

    first

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,14 +2,14 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
-  
+
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
   end
-	
+
   def self.search_for(content, method)
     if method == 'perfect'
       Book.where(title: content)
@@ -21,4 +21,7 @@ class Book < ApplicationRecord
       Book.where('title LIKE ?', '%'+content+'%')
     end
   end
+
+  scope :created_today, -> { where(created_at: Time.zone.now.all_day) } # 今日
+  scope :created_yesterday, -> { where(created_at: 1.day.ago.all_day) } # 前日
 end
